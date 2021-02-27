589625b75e56e2375f6993584199674d89df7524
sakebook <sakebook@gmail.com>
2021-02-27 13:17:06 +0900

    Add dirty puppy and list

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,11 +18,19 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.data.Puppy
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -40,7 +48,18 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun MyApp() {
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        Column {
+            TopAppBar(title = { Text(text = "Puppies🐶") })
+            LazyColumn {
+                items(getPuppies()) { item ->
+                    Text(
+                        text = "Ready... Set... GO! ${item.name}",
+                        modifier = Modifier.padding(8.dp),
+                        style = MaterialTheme.typography.body1,
+                    )
+                }
+            }
+        }
     }
 }
 
@@ -58,4 +77,8 @@ fun DarkPreview() {
     MyTheme(darkTheme = true) {
         MyApp()
     }
+}
+
+private fun getPuppies(): List<Puppy> {
+    return (0 until 10).map { Puppy("name: ${it}", "description: ${it}", it, "kind: ${it}") }
 }
